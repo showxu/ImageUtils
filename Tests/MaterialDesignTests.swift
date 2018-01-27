c3eea0a34979e1348b6cf47276272b9ca13e6865
0xxd0 <10173746+0xxd0@users.noreply.github.com>
2018-01-28 01:13:25 +0800

    Update test

--- Tests/MaterialDesignTests.swift
+++ Tests/MaterialDesignTests.swift
@@ -23,30 +23,13 @@ class MaterialDesignTests: XCTestCase {
                               in: Bundle(for: MaterialDesignTests.self),
                               compatibleWith: nil)
         let palette = Palette.from(preview!).generate()
-
+        let swatches = palette.getSwatches()
         let vibrant = Color(hex: palette.getVibrantColor(0))
-        let vibrantView = UIView(frame: CGRect.init(x: 0, y: 0, width: 200, height: 200))
-        vibrantView.backgroundColor = vibrant
-        
         let vibrantDark = Color(hex: palette.getDarkVibrantColor(0))
-        let vibrantDarkView = UIView(frame: CGRect.init(x: 0, y: 0, width: 200, height: 200))
-        vibrantDarkView.backgroundColor = vibrantDark
-        
         let vibrantLight = Color(hex: (palette.getLightVibrantColor(0)))
-        let vibrantLightView = UIView(frame: CGRect.init(x: 0, y: 0, width: 200, height: 200))
-        vibrantLightView.backgroundColor = vibrantLight
-        
         let muted = Color(hex: palette.getMutedColor(0))
-        let mutedView = UIView(frame: CGRect.init(x: 0, y: 0, width: 200, height: 200))
-        mutedView.backgroundColor = muted
-        
         let mutedDark = Color(hex: palette.getDarkMutedColor(0))
-        let mutedDarkView = UIView(frame: CGRect.init(x: 0, y: 0, width: 200, height: 200))
-        mutedDarkView.backgroundColor = mutedDark
-        
         let mutedLight = Color(hex: palette.getLightMutedColor(0))
-        let mutedLightView = UIView(frame: CGRect.init(x: 0, y: 0, width: 200, height: 200))
-        mutedLightView.backgroundColor = mutedLight
     }
     
     func testPerformanceExample() {
