48c8c47af2cc13a48b70b97cf07d4bc3baee40c5
0xxd0 <10173746+0xxd0@users.noreply.github.com>
2018-01-28 01:53:15 +0800

    Add phases

--- Spotlight.playground/Pages/Introduce.xcplaygroundpage/Contents.swift
+++ Spotlight.playground/Pages/Introduce.xcplaygroundpage/Contents.swift
@@ -3,16 +3,19 @@
  > # ⚠️
  > To use **Spotlight.playground**, ImageUtils.framework is necessary:
  1. Open **ImageUtils.xcworkspace**.
- 1. Build the **ImageUtils iOS** scheme (**Product** → **Build**).
- 1. Open **ImageUtils** playground in the **Project navigator**.
- 1. Show the Debug Area (**View** → **Debug Area** → **Show Debug Area**).
- 
+ 2. Build the **ImageUtils iOS** scheme (**Product** → **Build**).
+ 3. Open **ImageUtils** playground in the **Project navigator**.
+ 4. Show the Debug Area (**View** → **Debug Area** → **Show Debug Area**).
+ 5. Open the Markup render (**Editor** → **Show Rendered Markup**).
+ 6. Run (**Editor** → **Excute Playground**)
  ---
- ImageUtils is a cross-platform written in pure Swift for image processing and other useful toolkit, include
+ ImageUtils is a cross-platform written in pure Swift for image processing and other useful toolkit.
 
  ## Features:
  1. [vImage](vImage)
-
+     - Image Effects
+ 2. [Material Design](Material\ Design)
+     - Android Palette
  */
 
 //: [Next Page](@next)
